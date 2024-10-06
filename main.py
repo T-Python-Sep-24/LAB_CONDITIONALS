b3afd4b06d851e6efb458f6d5f9ce99cc8ae79d5
Eyad <eyalghamdi@outlook.com>
2024-10-06 12:37:50 +0300

    all worl done

---
+++ main.py
@@ -0,0 +1,13 @@
+movie = "titanic"
+rateing = 3
+popularity=72.65
+
+
+if rateing >= 4 and popularity > 80 :
+    print("Highly recommended")
+elif rateing >= 3 and popularity > 70:
+    print("I recommended it . It is good")
+elif rateing >= 2 and popularity > 60:
+    print("You should check it out!")
+else:
+    print("Don't watch it. It is a waste of time")
